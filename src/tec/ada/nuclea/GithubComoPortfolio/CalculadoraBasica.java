ccfd042a71b7abde94fe464ba8878a2394e75527
Ademar Moreira <ademar.moreira@gmail.com>
2024-01-29 22:35:25 -0300

    update

---
+++ src/tec/ada/nuclea/GithubComoPortfolio/CalculadoraBasica.java
@@ -0,0 +1,11 @@
+package tec.ada.nuclea.GithubComoPortfolio;
+
+import java.util.Scanner;
+
+public class CalculadoraBasica {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+
+    }
+}
